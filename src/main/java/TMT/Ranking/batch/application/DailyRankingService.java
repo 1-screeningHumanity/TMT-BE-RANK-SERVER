ea54b143ec3ba1635b6c974ffd1f7cf0346ff8ea
[hoontaepark] <pthomj13@gmail.com>
2024-06-20 14:13:36 +0900

    Feat:랭킹 집계 구현

--- src/main/java/TMT/Ranking/batch/application/DailyRankingService.java
+++ src/main/java/TMT/Ranking/batch/application/DailyRankingService.java
@@ -1,10 +1,6 @@
-//package TMT.Ranking.batch.application;
-//
-//import TMT.Ranking.batch.domain.DailyRanking;
-//import TMT.Ranking.daliywallet.domain.DailyWallet;
-//import java.time.LocalDateTime;
-//
-//public interface DailyRankingService {
-//
-//    DailyRanking createprofit(DailyWallet dailyWallet);
-//}
+package TMT.Ranking.batch.application;
+
+public interface DailyRankingService {
+
+
+}
--- src/main/java/TMT/Ranking/batch/application/DailyRankingServiceImp.java
+++ src/main/java/TMT/Ranking/batch/application/DailyRankingServiceImp.java
@@ -1,21 +1,27 @@
-//package TMT.Ranking.batch.application;
-//
-//import TMT.Ranking.batch.domain.DailyRanking;
-//import TMT.Ranking.batch.infrastructure.DailyRankingRepository;
-//import TMT.Ranking.daliywallet.domain.DailyWallet;
-//import TMT.Ranking.daliywallet.infrastructure.DailyWalletRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class DailyRankingServiceImp implements DailyRankingService {
-//
-//    private final DailyWalletRepository dailyWalletInfoRepository;
-//    private final DailyRankingRepository dailyRankingRepository;
-//
+package TMT.Ranking.batch.application;
+
+import TMT.Ranking.batch.infrastructure.DailyRankingRepository;
+import TMT.Ranking.daliywallet.infrastructure.DailyWalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DailyRankingServiceImp implements DailyRankingService {
+
+    private final DailyWalletRepository dailyWalletInfoRepository;
+    private final DailyRankingRepository dailyRankingRepository;
+
+
+
+//            //chunk의 타입이 List<?extends> 이기때문
+//            List<? extends DailyRankingDto> itemList = items.getItems();
+//            itemList.sort(Comparator.comparing(DailyRankingDto::getProfit).reversed());
+//            Long rank = 0L;
+
+
 //    @Override
 //    public DailyRanking createprofit(DailyWallet dailyWallet) {
 //        return null;
 //    }
-//}
+}
